e42b01b674a4e1f30c1cba4817f25b65981a4b4f
Aaron Brady <aleebrady@gmail.com>
2020-08-30 00:27:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,18 +5,6 @@
 require 'pry'
 
 class EmailAddressParser
-  attr_accessor :emails
-  
-  def initialize(emails)
-    @emails = emails 
-  end
-  
-  def parse
-    emails.split.map do |email|
-      email.split(",")
-    end
-    .flatten.uniq
-end
-    
+ 
   
 end
